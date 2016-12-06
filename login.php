c24ff4033c1d1f9a59a7072a2ade1223a0939dab
zhangbailong945 <1207549344@qq.com>
2016-12-06 23:48:35 +0800

    update zblShop UI.

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>ZBL购物商场</title>
+<link type="text/css" rel="stylesheet" href="./plugins/bootstrap/css/bootstrap.min.css"/>
+<link type="text/css" rel="stylesheet" href="./resources/css/style.css"/>
+<link type="text/css" rel="stylesheet" href="./plugins/navcar/css/nav.css"/>
+<link type="text/css" rel="stylesheet" href="./plugins/layer/skin/default/layer.css"/>
+<link type="text/css" rel="stylesheet" href="./resources/css/signin.css"/>
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" role="form" action="index.php">
+        <h2 class="form-signin-heading">登录ZBL商城</h2>
+        <input type="text" class="form-control" placeholder="输入账号" required autofocus>
+        <input type="password" class="form-control" placeholder="输入密码" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me">记住我
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">登录</button>
+      </form>
+
+    </div> <!-- /container -->
+<script type="text/javascript" src="./plugins/jquery/jquery-3.1.1.js"></script>
+<script type="text/javascript" src="./plugins/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="./plugins/flycar/js/jquery.fly.min.js"></script>
+<script type="text/javascript" src="./plugins/navcar/js/nav.js"></script>
+<script type="text/javascript" src="./plugins/layer/layer.js"></script>
+  </body>
+</html>
